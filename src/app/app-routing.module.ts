518cafc7aae2c20188ed2ad6ed25884cb31c82c4
giriui <giriuidevelop@gmail.com>
2021-04-15 02:54:55 +0530

    service page init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,11 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },  {
+    path: 'services',
+    loadChildren: () => import('./services/services.module').then( m => m.ServicesPageModule)
   }
+
 
 
 
---
+++ src/app/services/services.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ServicesPageRoutingModule } from './services-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
+import { ServicesPage } from './services.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ServicesPageRoutingModule,
+    TranslateModule
+  ],
+  declarations: [ServicesPage]
+})
+export class ServicesPageModule {}
